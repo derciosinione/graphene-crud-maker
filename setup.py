26111e5918434c99439fcbfd3842154fcab49536
dercio-sinione <derciosinione@gmail.com>
2021-05-14 18:13:30 +0100

    Changes

--- setup.py
+++ setup.py
@@ -8,18 +8,23 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='graphene_crud_maker',
+    name='graphene-crud-maker',
     version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',  # example license
     description='This is a project to auto generate a graphql crud using graphene django',
     long_description=README,
+    long_description_content_type="text/markdown",
     url='https://github.com/dercio-sinione/graphene-crud-maker',
+    project_urls={
+        "Bug Tracker": "https://github.com/dercio-sinione/graphene-crud-maker/issues",
+    },
     author='Dércio Sinione',
     author_email='derciosinione@gmail.com',
     classifiers=[
-        'Environment :: Web Environment',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
         'Framework :: Django',
         'Framework :: Django :: 1.11',  # replace "X.Y" as appropriate
         'Intended Audience :: Developers',
@@ -33,7 +38,10 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        "Programming Language :: Python :: Implementation :: PyPy",
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
+    python_requires=">=3",
 )
